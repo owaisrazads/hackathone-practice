65de2dd392639d717d2df2b34a6adae68a8bd60c
unknown <oraza7969@gmail.com>
2024-01-10 04:38:36 -0800

    hackathone practice

---
+++ Ecommerce-website/config.js
@@ -0,0 +1,18 @@
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAaRKSJanhz6e5cHm5lCiKimcl16UFH-bk",
+  authDomain: "practice-firebase-d8c41.firebaseapp.com",
+  projectId: "practice-firebase-d8c41",
+  storageBucket: "practice-firebase-d8c41.appspot.com",
+  messagingSenderId: "392425559970",
+  appId: "1:392425559970:web:169eb3a487089f33f2c4fb",
+  measurementId: "G-TDXSP4TX68"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
---
+++ Ecommerce-website/login.js
@@ -0,0 +1,11 @@
+// console.log('Hello Worker');
+const form = document.querySelector('#form')
+const email = document.querySelector('#email')
+const password = document.querySelector('#password')
+
+
+form.addEventListener('submit', (event)=>{
+    event.preventDefault()
+    console.log(email.value);
+    console.log(password.value);
+})
